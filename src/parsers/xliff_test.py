e4986ef00029f91d20f5cfb57c4dfcb8d7b86aec
Max Kanat-Alexander <avatraxiom@gmail.com>
2025-07-26 01:13:42 -0400

    Split the SDLXLIFF test into multiple tests.

--- src/parsers/xliff_test.py
+++ src/parsers/xliff_test.py
@@ -63,46 +63,41 @@ def test_to_file_roundtrip(xliff_file_path, tmp_path):
         assert str(u1.target) == str(u2.target)
 
 
-def test_parse_sdlxliff(sdl_xliff_file_path):
+def test_sdlxliff_basic_metadata(sdl_xliff_file_path):
     doc = XliffDocument.from_file(sdl_xliff_file_path)
     assert doc.version == "1.2"
     assert doc.xmlns == "urn:oasis:names:tc:xliff:document:1.2"
 
-    files = list(doc.get_files())
-    assert len(files) == 1
-    file_elem = files[0]
-    assert file_elem.attrib["original"] == "test.txt"
-    assert file_elem.attrib["source-language"] == "en-US"
-    assert file_elem.attrib["target-language"] == "es-ES"
-    assert file_elem.attrib["datatype"] == "x-sdlfilterframework2"
 
+def test_sdlxliff_unit_special_markup(sdl_xliff_file_path):
+    doc = XliffDocument.from_file(sdl_xliff_file_path)
     units = list(doc.get_translation_units())
     assert len(units) == 2
 
-    # First translation unit
-    u1 = units[0]
-    # Source with emphasis tag
-    assert u1.source.text == "Hello, "
-    g = u1.source.getchildren()[0]
+    # Source emphasis tag
+    assert units[0].source.text == "Hello, "
+    g = units[0].source.getchildren()[0]
     assert g.tag.split("}")[-1] == "g"
     assert g.attrib["ctype"] == "x-html-em"
     assert g.text == "world"
     assert g.tail == "!"
-    # Target with emphasis tag
-    assert u1.target.text == "¡Hola, "
-    gt = u1.target.getchildren()[0]
+    # Target emphasis tag
+    assert units[0].target.text == "¡Hola, "
+    gt = units[0].target.getchildren()[0]
     assert gt.tag.split("}")[-1] == "g"
     assert gt.attrib["ctype"] == "x-html-em"
     assert gt.text == "mundo"
     assert gt.tail == "!"
-    assert str(u1.note) == "Greeting with emphasis"
+    # Note element
+    assert str(units[0].note) == "Greeting with emphasis"
 
-    alt = u1.find("alt-trans")
+
+def test_sdlxliff_first_unit_alt_trans(sdl_xliff_file_path):
+    doc = XliffDocument.from_file(sdl_xliff_file_path)
+    units = list(doc.get_translation_units())
+    assert len(units) == 2
+    # Alternative translations block
+    alt = next((e for e in units[0].getchildren() if e.tag.split("}")[-1] == "alt-trans"), None)
     assert alt is not None
     assert str(alt.source) == "Hello, world!"
     assert str(alt.target) == "Hola mundo!"
-
-    # Second translation unit
-    u2 = units[1]
-    assert "This is a test document" in str(u2.source)
-    assert str(u2.target) == ""
